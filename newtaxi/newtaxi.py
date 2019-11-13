577d212594313b4db450af78caae5cbfed7f3172
tw2665 <55668073+tw2665@users.noreply.github.com>
2019-11-13 11:41:30 -0500

    envtest upgrade

--- newtaxi/newtaxi.py
+++ newtaxi/newtaxi.py
@@ -6,6 +6,7 @@ from gym.envs.toy_text import discrete
 from gym import Env, spaces
 from gym.utils import seeding
 import numpy as np
+from termcolor import colored, cprint
 
 
 class NewTaxi(Env):
@@ -16,17 +17,24 @@ class NewTaxi(Env):
         self.m = m
         # locs is a tuple ([get on], [get off])
         self.locs = locs
-        self.state = [np.random.choice(n), np.random.choice(m)]
+        # the first is location parameter, the second is loaded with passenger 
+        self.state = {'loc':[np.random.choice(n), np.random.choice(m)], 'load':0}
         self.action_space = {'U': -1, 'D': 1, 'L': -1, 'R': 1}
+        # set up obs and act space size for q-table
+        self.action_space_n = len(self.action_space)
+        self.observation_space_n = n*m*len(locs)
         self.reward = 0
-        self.loaded = None
+        self.loaded = 0
+    
+    def state2state_id(self, state):
+        return (state['loc'][0]*self.n + state['loc'][1]) + (state['load'] * self.n * self.m)
         
     def reset(self):
         n, m = self.n, self.m
         self.env = np.zeros((n, m))
-        self.state = [np.random.choice(n), np.random.choice(m)]
+        self.state = {'loc':[np.random.choice(n), np.random.choice(m)], 'load':0}
         self.reward = 0
-        self.loaded = None
+        self.loaded = False
         return self.state
     
     def step(self, action):
@@ -35,32 +43,40 @@ class NewTaxi(Env):
         load = self.loaded
         reward = self.reward
         old_state = self.state
+        state = {}
         # update new state
         if action == 'U' or action == 'D':             
-            state = [old_state[0] + acts[action], old_state[1]]
+            state['loc'] = [old_state['loc'][0] + acts[action], old_state['loc'][1]]
         elif action == 'L' or action == 'R':
-            state = [old_state[0], old_state[1] + acts[action]]
+            state['loc'] = [old_state['loc'][0], old_state['loc'][1] + acts[action]]
         
         # check if hit the wall 
-        if state[0] < 0 or state[0] >= n or state[1] < 0 or state[1] >= m:
+        if state['loc'][0] < 0 or state['loc'][0] >= n or \
+                state['loc'][1] < 0 or state['loc'][1] >= m:
             reward -= 4 # total lost of reward should be this + 1 
             state = old_state 
         
         done = False
         if load:
-            if state == self.locs[1]:
+            state['load'] = 1
+            if state['loc'] == self.locs[1]:
                 reward += 5
                 done = True
             else: 
                 reward -= 1
-        elif load is None:
-            if state == self.locs[0]:
+        else:
+            # check if pick up the passenger
+            if state['loc'] == self.locs[0]:
                 reward += 5
+                load = 1
+                state['load'] = 1
             else: 
                 reward -= 1
+                state['load'] = 0
         
         self.state = state
         self.reward = reward
+        self.loaded = load
         return state, reward, done # no info return here
 
 
@@ -68,7 +84,8 @@ class NewTaxi(Env):
         locs = self.locs
         state = self.state
         env = self.env.copy()
-        env[state[0], state[1]] = 1 # taxi pos
+        load = state['load'] 
+        env[state['loc'][0], state['loc'][1]] = 1 # taxi pos
         env[locs[0][0], locs[0][1]] = 2 # get on point
         env[locs[1][0], locs[1][1]] = 3 # get off point
         n, m = self.n, self.m
@@ -79,9 +96,15 @@ class NewTaxi(Env):
                 if env[i, j] == 0:
                     out += '*'
                 elif env[i, j] == 1:
-                    out += 'X'
+                    if load == 1:
+                        out += colored('X', 'red')
+                    else:
+                        out += colored('X', 'yellow')
                 elif env[i, j] == 2:
-                    out += 'I'
+                    if load == 1:
+                        out += colored('I', 'red')
+                    else:
+                        out += colored('I', 'green')
                 elif env[i, j] == 3:
                     out += 'O'
                 out += '  '
